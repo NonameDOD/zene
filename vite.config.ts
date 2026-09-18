1baaa53552d66536c85dae4f8ccb65eb65bd149f
Fóti Dávid Bálint <foti.david@akk.lan>
2026-09-18 11:37:22 +0200

    Configure GitHub Pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/zene/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
+
